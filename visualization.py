11a2805f4af60a12f6b7cc4bc9109b2e8ceaa39f
Gianna G <gggmullon444@gmail.com>
2023-11-15 20:56:22 +0000

    Changed color and names

--- visualization.py
+++ visualization.py
@@ -2,18 +2,18 @@ import plotly.graph_objects as go
 
 import pandas as pd
 
-df = pd.read_csv('url goes here')
+df = pd.read_csv('https://github.com/marestaing/hosting/blob/main/visited_states.csv?raw=true')
 
 fig = go.Figure(data=go.Choropleth(
     locations=df['code'], # Spatial coordinates
     z = df['number students'].astype(float), # Data to be color-coded
     locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = 'Blues',
-    colorbar_title = "color scale title",
+    colorscale = 'tropic',
+    colorbar_title = "More people have visited",
 ))
 
 fig.update_layout(
-    title_text = 'title here',
+    title_text = 'How Many Kids in App Academy have visited Certain States',
     geo_scope='usa', # limite map scope to USA
 )
 
